3e56ca48246f0ad2e6453e0a170cc42f19e1256f
Bomi-kwon <bbomicoding@gmail.com>
2023-03-09 21:53:00 +0900

    백준 2869 달팽이는 올라가고 싶다

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,35 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
-
-		if (N == 1) {
-			System.out.println(1);
-		} 
-		else {
-			for (int i = 1; i <= 20000; i++) {
-				if (N > get_last_term(i) && N <= get_last_term(i + 1)) {
-					System.out.println(i + 1);
-					break;
-				}
-			}
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+		
+		int distance_after_firstday = V-A;
+		int distance_per_day = A-B;
+		int days;
+		
+		if(distance_after_firstday % distance_per_day == 0) {
+			days = distance_after_firstday / distance_per_day + 1;
 		}
-
-	}
-
-	private static int get_last_term(int i) {
-		return (int) (3 * Math.pow(i, 2)) - 3 * i + 1;
+		else {
+			days = distance_after_firstday / distance_per_day + 2;
+		}
+		
+		String str = Integer.toString(days);
+		bw.write(str);
+		bw.flush();
+		bw.close();
 	}
 }
