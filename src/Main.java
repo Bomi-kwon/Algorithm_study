912e6e00d4605daf68b2156f5bf4b27c23147991
Bomi-kwon <bbomicoding@gmail.com>
2023-03-11 22:53:40 +0900

    백준 25314 코딩은 체육과목입니다

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		
-		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int N = Integer.parseInt(bf.readLine());
 		
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
+		for(int i = 1 ; i <= N/4 ; i++) {
+			System.out.printf("long ");
+		}
+		System.out.printf("int");
 		
-		System.out.println(A+B+C);
 	}
 }
