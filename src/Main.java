3868a555bc114003ec5d1c42fce24816189b7e83
Bomi-kwon <bbomicoding@gmail.com>
2023-03-12 23:29:32 +0900

    백준 5086 배수와약수 (실패)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int A;
+		int B;
 		
-		int N = Integer.parseInt(bf.readLine());
-		
-		for(int i = 1 ; i <= N/4 ; i++) {
-			System.out.printf("long ");
+		while (true) {
+			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			
+			if (A == 0 && B == 0) {
+				break;
+			}
+			else if (B%A == 0 && A%B != 0) {
+				System.out.println("factor");
+				continue;
+			}
+			else if (A%B == 0 && B%A != 0) {
+				System.out.println("multiple");
+				continue;
+			}
+			else if (A%B != 0 && B%A != 0) {
+				System.out.println("neither");
+				continue;
+			}
+			
 		}
-		System.out.printf("int");
+		
 		
 	}
 }
