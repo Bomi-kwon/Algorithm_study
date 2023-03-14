9dbea11ffa72a284277b166476b14316d64b466e
Bomi-kwon <bbomicoding@gmail.com>
2023-03-14 21:36:03 +0900

    백준 10250 ACM 호텔

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,36 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int numbers_of_divisor = 0;
+		int T = Integer.parseInt(bf.readLine());
 		
+		int[] arr_H = new int[T];
+		int[] arr_W = new int[T];
+		int[] arr_N = new int[T];
 		
-		for (int i = 1 ; i <= N ; i++) {
-			if (N % i == 0) {
-				numbers_of_divisor++;
-				if (numbers_of_divisor == K) {
-					System.out.println(i);
-					break;
-				}
+		for(int i = 0 ; i < T ; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			arr_H[i] = Integer.parseInt(st.nextToken());
+			arr_W[i] = Integer.parseInt(st.nextToken());
+			arr_N[i] = Integer.parseInt(st.nextToken());
+		}
+		int floor;
+		int room;
+		
+		for(int i = 0 ; i < T ; i++) {
+			if(arr_N[i] % arr_H[i] == 0) {
+				floor = arr_H[i];
+				room = arr_N[i] / arr_H[i];
 			}
+			else {
+				floor = arr_N[i] % arr_H[i];
+				room = arr_N[i] / arr_H[i] + 1;
+			}
+			System.out.println(floor*100 + room);
 		}
-		if (numbers_of_divisor < K) {
-			System.out.println(0);
-		}
+		
+		
+		
 		
 	}
 }
